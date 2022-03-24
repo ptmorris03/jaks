deadcca41b4f5ff51ba3048c8e43c4af41874b2b
Paul Morris <pmorris2012@fau.edu>
2022-03-23 20:19:18 -0400

    Update __init__.py

--- jaks/modules/__init__.py
+++ jaks/modules/__init__.py
@@ -1,5 +1,5 @@
 from .core import Module
 from .transform import Translate, Scale, DotProduct, Rotate
 from .nonlin import RELU, QuickGELU, ZScore, ScaledDotProductAttention
-from .wrapper import Residual, ResNetStack, Loop
+from .wrapper import Residual, ResNetStack, Loop, Vmap
 from .layer import Linear, LayerNorm, MLP, MultiHeadAttention
